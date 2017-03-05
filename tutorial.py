6cf3e13fb8bc2e76ccadc7448384ca469875ccc2
benmiller314 <benmiller314@gmail.com>
2017-03-04 22:26:01 -0500

    database model
    Person, Institution (still same as School), User, UpdateLog,
    DataSource, Tag

--- tutorial.py
+++ tutorial.py
@@ -2,8 +2,11 @@ from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap
 from flask_wtf import Form
 from wtforms import StringField, PasswordField
+from py2neo import Graph, Node, Relationship, authenticate, watch
+from py2neo.ogm import GraphObject, Property, RelatedTo, RelatedFrom
+from os import getenv
 
-
+########### setup ########### 
 app = Flask(__name__)
 Bootstrap(app)
 app.config['SECRET_KEY'] = 'ithinkiwassupposedtosaysomething'
@@ -23,7 +26,87 @@ def person(name):
 	name_joined = title_except(name_split)
 	return '<h2>This is the page for ' + name_joined + '</h2>'
 
-########### classes ########### 
+
+########### models ########### 
+### database model ###
+
+class Person(GraphObject):
+    __primarykey__ = "name"
+	
+    name = Property()
+	
+    mentored = RelatedTo("Person")
+    mentored_by = RelatedFrom("Person")
+    worked_alongside = Related("Person")
+    studied_at = RelatedTo("Institution")
+    worked_at = RelatedTo("Institution")
+    tagged = RelatedTo("Tag")
+    member_of = RelatedTo("Institution")
+    
+    last_update = RelatedTo("UpdateLog")
+     
+    def __lt__(self, other):
+        return self.name < other.name
+
+class Institution(GraphObject):
+	__primarykey__ = "name"
+	
+	name = Property()
+	location = Property()
+	type = Property()
+	carnegie_class = Property()
+
+    students = RelatedFrom("Person", "STUDIED_AT")
+    employees = RelatedFrom("Person", "WORKED_AT")
+    members = RelatedFrom("Person", "MEMBER_OF")
+	
+    last_update = RelatedTo("UpdateLog")
+	
+    def __lt__(self, other):
+        return self.name < other.name
+
+
+class User(GraphObject)
+	__primarykey__ = "username"
+	
+	username = Property()
+	joined = Property()
+	last_access = Property()
+	blocked = Property()
+	
+	contributed = RelatedTo("UpdateLog")
+	
+class UpdateLog(GraphObject)
+	__primarykey__ = "id"
+	
+	id = Property()
+	timestamp = Property()
+	query = Property()
+	
+	previous = relatedTo("UpdateLog", "LAST_UPDATE")
+	next = relatedFrom("UpdateLog", "LAST_UPDATE")
+	based_on = relatedTo("DataSource")
+	
+	
+class DataSource(GraphObject)
+	__primarykey__ = "id"
+	
+	id = Property()
+	description = Property()
+	
+	based_on = relatedFrom("UpdateLog")
+
+class Tag(GraphObject)
+	__primarykey__ = "name"
+	
+	name = Property()
+	description = Property()
+	
+	see_also = Related("Tag")
+	tagged = RelatedFrom("Person")
+	
+
+### forms ###
 class SearchForm(Form):
 	name = StringField('name')
 	
