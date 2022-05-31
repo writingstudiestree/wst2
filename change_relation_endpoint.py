885050e93869d9c741838d325c4832cd697d08a3
benmiller314 <benmiller314@gmail.com>
2022-05-31 12:03:55 -0400

    no content changes
    copy to local WST folder

--- change_relation_endpoint.py
+++ change_relation_endpoint.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
 ###### change_relation_endpoint ######
-# 
-# GOAL: Given an existing relation (n1)-[r]->(n2), 
+#
+# GOAL: Given an existing relation (n1)-[r]->(n2),
 # 		get to (n1)-[copy-of-r]->(n3)
-# USES: If you discover a duplicate or misspelled name in (n2), 
-#		and want to transfer its relationships to the original 
+# USES: If you discover a duplicate or misspelled name in (n2),
+#		and want to transfer its relationships to the original
 #		or correctly spelled (n3).
 ######
 
@@ -18,18 +18,18 @@ graph = Graph(password=getenv('NEO4J_PASSWORD'))
 watch('neo4j.bolt')
 
 
-
+# test values
 current_target = "Purdue University at West Lafayette"
 new_target = "Purdue University"
 
 find_query = '''
 match (i {name: "%s"})<-[r]-(p)
 with p, r, i
-return p.name as source, 
+return p.name as source,
 LABELS(i) as targ_labels,
-TYPE(r) as reltype, 
+TYPE(r) as reltype,
 properties(r) as rel_props,
-i.name as current_target, 
+i.name as current_target,
 j.name as new_target
 '''  % (current_target, new_target)
 reply = graph.run(find_query).data()[:1]
@@ -41,21 +41,21 @@ for record in reply:
 	source = record['source']
 	current_target = record['current_target']
 	new_target = record['new_target']
-		
+
 	change_query = '''
 	merge (j:%s {name: "%s"})<-[r:%s]-(p:%s {name: "%s"})
 	''' % new_target
-	
-	
+
+
 	'''
 	match (i:Institution {name: "%s"})<-[r]-(p {name: "%s"})
 	with p, r, i
-	match (j:Institution {name:"%s"}) 
+	match (j:Institution {name:"%s"})
 	with p, r, i, j
 	merge (p)-[q:%s]->(j)
 		on create set q += %s
-	return p.name as source, 
-	r as old_relation, 
+	return p.name as source,
+	r as old_relation,
 	q as new_relation,
 	j.name as new_target,
 	TYPE(r) as old_reltype,
