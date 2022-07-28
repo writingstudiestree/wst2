8890e975c5729b30553703a9830bcd5e178b1d5a
James Fenn <me@jfenn.me>
2022-07-28 13:36:24 -0400

    add homepage nav template + login form

---
+++ src/components/Modal.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	export let title: string = "";
+	export let show: boolean = false;
+</script>
+
+{#if show}
+<div class="modal show" tabindex="-1" aria-modal="true" role="dialog">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title">{title}</h5>
+				<a href="./#" role="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
+					<span class="d-none">Close</span>
+				</a>
+			</div>
+			<div class="modal-body">
+				<slot/>
+			</div>
+			{#if $$slots.footer}
+			<div class="modal-footer">
+				<slot name="footer"/>
+			</div>
+			{/if}
+		</div>
+	</div>
+</div>
+{/if}
+
+<style lang="scss">
+.modal.show {
+	display: block;
+	background-color: rgba(0, 0, 0, 0.5);
+}
+</style>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,61 @@
 <script>
-  import "../app.scss";
+	import { page } from "$app/stores";
+	import "../app.scss";
+
+	import Modal from "../components/Modal.svelte";
+
+	const links = [{
+		type: 'link',
+		name: 'Home',
+		url: '/',
+	},{
+		type: 'link',
+		name: 'Explore',
+		url: '/explore/',
+	}];
 </script>
 
-<slot />
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+	<div class="container">
+		<a class="navbar-brand" href="/">Writing Studies Tree</a>
+
+		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+
+		<div class="collapse navbar-collapse" id="navbarSupportedContent">
+			<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+				{#each links as link (link.url)}
+				<li class="nav-item">
+					<a class={`nav-link ${$page.url.pathname === link.url ? 'active' : ''}`} href={link.url}>{link.name}</a>
+				</li>
+				{/each}
+			</ul>
+
+			<a href="#signin" class="btn btn-primary" role="button">Sign In</a>
+		</div>
+	</div>
+</nav>
+
+<Modal title="Sign In" show={$page.url.hash === "#signin"}>
+	<form id="signIn">
+		<div class="mb-3">
+			<label for="signInEmail" class="form-label">Email address</label>
+			<input type="email" class="form-control" id="signInEmail" placeholder="name@example.com">
+		</div>
+
+		<div class="mb-3">
+			<label for="signInPassword" class="form-label">Password</label>
+			<input type="password" class="form-control" id="signInPassword">
+		</div>
+	</form>
+
+	<div slot="footer">
+		<a href="./#" type="submit" class="btn btn-secondary">Cancel</a>
+		<input type="submit" form="signIn" class="btn btn-primary" value="Sign In"/>
+	</div>
+</Modal>
+
+<main class="container">
+	<slot />
+</main>
