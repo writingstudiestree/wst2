c164df20935b3b6c354448286837d877734fc65a
James Fenn <me@jfenn.me>
2022-07-28 15:29:43 -0400

    add editorconfig for consistent indentation style

--- svelte.config.js
+++ svelte.config.js
@@ -3,19 +3,19 @@ import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://github.com/sveltejs/svelte-preprocess
-  // for more information about preprocessors
-  preprocess: [
-    preprocess({
-      scss: {
-        prependData: '@use "src/variables.scss" as *;',
-      },
-    }),
-  ],
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: [
+		preprocess({
+			scss: {
+				prependData: '@use "src/variables.scss" as *;',
+			},
+		}),
+	],
 
-  kit: {
-    adapter: adapter(),
-  },
+	kit: {
+		adapter: adapter(),
+	},
 };
 
 export default config;
