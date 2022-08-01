13072d7c99ea058363cb14e4c86d436adcdf759c
James Fenn <me@jfenn.me>
2022-08-01 14:57:09 -0400

    add insert functions for content/relations + database "mock" for integration tests

---
+++ __mocks__/mysql2/promise.ts
@@ -0,0 +1,28 @@
+import { readFile } from 'fs/promises';
+import getDatabase from '@databases/mysql-test';
+import type mysql2 from 'mysql2/promise';
+
+import { vi } from 'vitest';
+const mysql: typeof mysql2 = await vi.importActual("mysql2/promise");
+
+async function createConnection(...args: any[]): ReturnType<typeof mysql2.createConnection> {
+	// create a test database instance
+	const { databaseURL } = await getDatabase();
+	// connect to the provided database URL
+	const conn = await mysql.createConnection(databaseURL);
+
+	// initialize database tables with schema in "db/init.sql"
+	const initSql = await readFile("./db/init.sql", "utf-8");
+	for (let line of initSql.split(";")) {
+		// remove comment syntax
+		line = line.replace(/^--(.*)\n/, "");
+		line = line.trim();
+		if (!line) continue;
+
+		await conn.execute(line);
+	}
+
+	return conn;
+}
+
+export default { createConnection };
--- src/api/db.ts
+++ src/api/db.ts
@@ -1,14 +1,32 @@
 import mysql from 'mysql2/promise';
 import { DATABASE_URL } from '../utils/constants';
-import type { Content } from './types';
+import type { Content, Relations } from './types';
 
 const connection = await mysql.createConnection(DATABASE_URL);
 
-connection.connect();
+await connection.connect();
 
 export async function queryTest(): Promise<Content[]> {
 	const [rows, fields] = await connection.execute("SELECT * FROM content") as [Content[], any];
 
 	return rows;
+}
+
+export async function insertContent(content: Omit<Content, "id">): Promise<number> {
+	const [result] = await connection.execute(
+		"INSERT INTO content (type, name, content) VALUES (?, ?, ?);",
+		[content.type, content.name, content.content],
+	) as [mysql.ResultSetHeader, any];
+
+	return result.insertId;
+}
+
+export async function insertRelation(relation: Omit<Relations, "id">): Promise<number> {
+	const [result] = await connection.execute(
+		"INSERT INTO relations (type, subtype, link_from, link_to, year_start, year_end, content) VALUES (?, ?, ?, ?, ?, ?, ?);",
+		[relation.type, relation.subtype, relation.link_from, relation.link_to, relation.year_start, relation.year_end || null, relation.content],
+	) as [mysql.ResultSetHeader, any];
+
+	return result.insertId;
 }
 
