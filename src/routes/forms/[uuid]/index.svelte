160454cca5a952b844d21e6d33efd635c3da5324
Ben Adams <bma54@pitt.edu>
2022-08-13 13:42:11 -0700

    continuing work on relationMaker

--- src/routes/forms/[uuid]/index.svelte
+++ src/routes/forms/[uuid]/index.svelte
@@ -3,6 +3,7 @@
 	import School from "src/components/forms/School.svelte";
 	import Institution from "src/components/forms/Institution.svelte";
 	import Citation from "src/components/forms/Citation.svelte";
+	import Preview from "src/components/viewingComponents/Preview.svelte";
 
 	import { isRecordType, InsertFormType } from 'src/api/forms/base';
 	import { page } from '$app/stores';
@@ -12,13 +13,15 @@
 	//Form Modules
 	import { goto } from "$app/navigation";
 	import { browser } from '$app/env';
+import RelationBasic from "src/components/relationIntakeComponents/RelationBasic.svelte";
 
+	let saveAndContinue = false;
 	function handleSubmit() {
 		if (validateForms())
 		{
 			const form = get(draftForm);
 			console.log(form);
-			goto(`./relationMaker`);
+			saveAndContinue = true;
 		}
 	}
 
@@ -27,7 +30,14 @@
 	}
 
 	const back = () => {
-		goto(`/forms`);
+		if (!saveAndContinue)
+		{
+			goto(`/forms`);
+		}
+		else
+		{
+			saveAndContinue = false;
+		}
 	};
 
 	$: form = $draftForm[$page.params.uuid]?.form;
@@ -36,20 +46,32 @@
 		goto("/forms");
 	}
 </script>
-<button on:click = {back} class = "btn btn-secondary">Back to menu</button>
+<button on:click = {back} class = "btn btn-secondary">Back to previous step</button>
 
 {#if form}
 	{#each $form as entry (entry.value.id)}
-		{#if isRecordType(entry, InsertFormType.CONTENT) }
+		{#if isRecordType(entry, InsertFormType.CONTENT)}
 			{#if entry.value.type === "person"}
 			<Person bind:value={entry.value} />
+			{#if saveAndContinue}
+			<h1 class = "textCenter">Form relationships</h1>
+			<div class = "inside">
+				<h2>Getting Ready</h2>
+				<p>Now that your node has been created, it is time to start forming relationships within the network! First, let's take a look at your current entry:</p>
+				<div class = "inception">
+					<h2>{entry.value.content}</h2>
+				</div>
+				<p>See any errors? Use the 'back' button at the top of the page to correct them before we start integrating your node in to the network!</p>
+			</div>
+			{/if}
 			{:else if entry.value.type === "school"}
 			<School bind:value={entry.value} />
 			{:else if entry.value.type === "institution"}
 			<Institution bind:value={entry.value} />
 			{/if}
-		{:else if isRecordType(entry, InsertFormType.CITATION) }
-			<Citation bind:value={entry.value}/>
+		{/if}
+		{#if isRecordType(entry, InsertFormType.RELATION) }
+			<p>test</p>
 		{/if}
 	{/each}
 
